9291e60c81f50e83e6e03dbb64fe452dcc32e416
John Knickerbocker <PokeMaster843@gmail.com>
2015-03-25 14:04:16 -0400

    Update lexer.rb

--- lexer.rb
+++ lexer.rb
@@ -24,22 +24,33 @@ class Lexer
         end
         i += identifier.size
       
-      elseif constant = chunk[/\A([A-Z]\w*)/, 1]
+      elsif constant = chunk[/\A([A-Z]\w*)/, 1]
         tokens << [:CONSTANT, constant]
         i += constant.size
       
-      elseif number = chunk[/\A([0-9]+)/, 1]
+      elsif number = chunk[/\A([0-9]+)/, 1]
         tokens << [:NUMBER, number.to_i]
         i += number.size
       
-      elseif string = chunk[/\A"([^"]*)"/, 1]
+      elsif string = chunk[/\A"([^"]*)"/, 1]
         tokens << [:STRING, string]
         i += string.size
       
-      elseif indent = chunk[/\A\:\n( +)/m, 1]
+      elsif indent = chunk[/\A\:\n( +)/m, 1]
         if indent.size <= current_indent
           raise "Bad indent level, got #{indent.size} indents, expected > #{current_indent}"
         end
-      
+        current_indent = indent.size
+        indent_stack.push(current_indent)
+        tokens << [:INDENT, indent.size]
+        i += indent.size + 2
+      elsif indent = chunk[/\A\n( *)/m, 1]
+        if indent.size == current_indent
+          tokens << [:NEWLINE, "\n"]
+        
+        elsif indent.size < current_indent
+          while indent.size < current_indent
+            indent_stack.pop
+    
     
   
