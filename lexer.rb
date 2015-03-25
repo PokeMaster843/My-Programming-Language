f637fc7a5cf558cbb6012ee3bc9cd5cf2ee77614
John Knickerbocker <PokeMaster843@gmail.com>
2015-03-25 14:46:16 -0400

    Update lexer.rb

--- lexer.rb
+++ lexer.rb
@@ -36,30 +36,29 @@ class Lexer
         tokens << [:STRING, string]
         i += string.size
       
-      elsif indent = chunk[/\A\:\n( +)/m, 1]
-        if indent.size <= current_indent
-          raise "Bad indent level, got #{indent.size} indents, expected > #{current_indent}"
-        end
-        current_indent = indent.size
-        indent_stack.push(current_indent)
-        tokens << [:INDENT, indent.size]
-        i += indent.size + 2
-      elsif indent = chunk[/\A\n( *)/m, 1]
-        if indent.size == current_indent
-          tokens << [:NEWLINE, "\n"]
-        
-        elsif indent.size < current_indent
-          while indent.size < current_indent
-            indent_stack.pop
-            current_indent = indent_stack.last || 0
-            tokens << [:DEDENT, indent.size]
-          end
-          tokens << [:NEWLINE, "\n"]
-        else
-          raise "Missing ':'"
-        end
-        i += indent.size + 1
-          
-        
+      elsif operator = chunk[/\A(\|\||&&|==|!=|<=|>=)/, 1]
+        tokens << [operator, operator]
+        i += operator.size
       
+      elsif chunk.match(/\A\n+/)
+        tokens << [:NEWLINE, "\n"]
+        i += 1
+        
+      elsif chunk.match(/\A /)
+        i += 1
+        
+      else
+        value = chunk[0,1]
+        tokens << [value, value]
+        
+      end  
+      
+    end
     
+    while indent = indent_stack.pop
+      tokens << [:DEDENT, indent_stack.first || 0]
+    end
+    
+    tokens
+  end
+end
